e550061d70b1697082cdeac72e82dd673d2ea6f6
vmc529440@gmail.com <Mtb8m?pPbEA?j7JF>
2022-10-27 00:00:24 +0700

    Refactor and fix unclickable see more div

--- main.py
+++ main.py
@@ -14,19 +14,6 @@ import random
 import undetected_chromedriver as uc
 driver = uc.Chrome(enable_cdp_events=True, use_subprocess=True, version_main=106)
 
-# chromedriver_autoinstaller.install()
-# options = webdriver.ChromeOptions()
-# options.add_argument('--save-page-as-mhtml')
-# options.add_argument('--disable-blink-features=AutomationControlled')
-# options.add_argument('--proxy-server=14.177.7.187:4003')
-# options.add_experimental_option("detach", True)
-# options.add_experimental_option("useAutomationExtension", False)
-# options.add_experimental_option("excludeSwitches", ["enable-automation"])
-#
-# options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36')
-# SEQUENCE = r"offline_webpage_infor_pages/page_source"
-# driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()), options=options)
-
 popular_investors = ["rallek", "miyoshi", "reinhardtcoetzee", "sharonconnolly", "cphequities", "rubymza", "richardstroud", "ingruc", "pino428", "triangulacapital", "jordenboer", "slow_and_steady", "monabel", "emge2116", "doopiecash", "magic_kaito", "ioatri",
        "balticseal", "daniel4653", "fifty-five", "calintrading", "sgstjc", "greatcompanies", "karlo_s", "lukaszkisicki", "trojaneto", "chiay0327", "carlos_delarosa", "trex8u247", "estebanopatril", "nintingale", "taherkamari", "zofesu",
       "andreamarcon16", "nestorarmstrong", "bhavesh_spx", "theosanders", "dividends_income", "andresvicunat", "tomwintjes", "nicoroumeau", "mjtfernandez", "myhungetoro", "maxdividend", "victorvatin",
@@ -47,73 +34,80 @@ fail_pages = ""
 name_check = ""
 rank_check = ""
 
-print("Begin crawling: " + str(len(popular_investors)))
+print("Begin crawling: " + str(len(popular_investors)) + " investors' data")
 seed = random.randrange(3, 8)
+
 for pi in popular_investors:
       time.sleep(seed * random.random())
+
+      # GET_PORTFOLIO_DATA #
+
+      # CHECK_WEBSITE #
       try:
-            driver.get("https://www.etoro.com/people/" + pi + "/portfolio")
+            url_string = "https://www.etoro.com/people/" + pi + "/portfolio"
+            driver.get(url_string)
       except WebDriverException as e:
-            print("No page: " + pi + " is not found")
-            print("Error: " + str(e))
+            print("NO_PAGE_FOUND: " + url_string)
+            print("ERROR: " + str(e))
             fail_pages += pi + ", "
             continue
+      # CHECK_WEBSITE #
 
       time.sleep(seed * random.random())
-      final_profile = pi
+
+      # DOWNLOAD_WEBSITE #
+      final_portfolio_profile = pi
       with open(directory + "/" + pi + ".html", "w", encoding='utf-8') as f:
             try:
                   f.write(driver.page_source)
             except WebDriverException as e:
-                  print("Cant save " + pi + ".html file")
-                  print("Error: " + str(e))
-                  fail_pages += pi + ", "
+                  print("CANT_SAVE_PI_PORTFOLIO_PAGE: " + pi)
+                  print("CANT_SAVE_PI_PORTFOLIO_PAGE_URL: " + url_string)
+                  print("ERROR: " + str(e))
+                  final_portfolio_profile += pi + ", "
+                  continue
+      # DOWNLOAD_WEBSITE #
 
+      # CHECK_PI_FULLNAME #
       try:
             name_check = driver.find_element(By.XPATH, "//h1[@automation-id='user-head-not-nickname']").text
+            print("check_pi_fullname: " + name_check)
       except NoSuchElementException:
             name_check = ""
+            print("FAILED_TO_GET_FULLNAME: " + pi)
+      # CHECK_PI_FULLNAME #
 
+      # CHECK_PI_FIRST_TICKET #
       try:
             first_ticket = driver.find_element(By.XPATH, "(//*[contains(@class,'i-portfolio-table-name-symbol')])[1]").text
       except NoSuchElementException:
             try:
                   first_ticket = driver.find_element(By.XPATH, "//h1[@automation-id='cd-public-portfolio-table-item-title']").text
+                  print("check_pi_first_ticket: " + first_ticket)
             except NoSuchElementException:
                   first_ticket = ""
+                  print("FAILED_TO_GET_FIRST_TICKET: " + pi)
+      # CHECK_PI_FIRST_TICKET #
 
-      print("name_check: " + name_check)
-      print("first_ticket: " + first_ticket)
 
       if len(name_check) != "" and len(first_ticket) != "":
             number_of_record += 1
       else:
             fail_pages += pi + ", "
-            print("failed crawl: " + pi)
+            print("FAILED_PI_PORTFOLIO: " + pi)
 
-
-
-      ###################################
       time.sleep(seed * random.random())
-      driver.find_element(By.XPATH, "//a[@automation-id='et-tab-stats']").click()
 
+      #GET_PERFORMANCE_DATA #
+
+      driver.find_element(By.XPATH, "//a[@automation-id='et-tab-stats']").click()
       directory = r"offline_webpage_infor_pages/" + datetime_now
       if not os.path.exists(directory):
             os.makedirs(directory)
 
-      # try:
-      #       driver.get("https://www.etoro.com/people/" + pi + "/stats")
-      #       time.sleep(1)
-      # except WebDriverException as e:
-      #       print("No page: " + pi + " is not found")
-      #       print("Error: " + str(e))
-      #       fail_pages += pi + ", "
-      #       continue
-
       try:
-            button_see_more = driver.find_element(By.XPATH,"//span[@automation-id='cd-user-stats-performance-chart-show-more-text']")
-            button_see_more.click()
-            print(button_see_more.click())
+            button_see_more = driver.find_element(By.XPATH, "//span[@automation-id='cd-user-stats-performance-chart-show-more-text']")
+            driver.execute_script("arguments[0].click();", button_see_more)
       except NoSuchElementException as e:
             print("No element is found")
             print("Error: " + str(e))
@@ -122,6 +116,8 @@ for pi in popular_investors:
             print("Error: " + str(e))
 
       time.sleep(seed * random.random())
+
+
       final_profile = pi
       with open(directory + "/" + pi + ".html", "w", encoding='utf-8') as f:
             try:
@@ -132,7 +128,7 @@ for pi in popular_investors:
                   fail_pages += pi + ", "
 
       try:
-            name_check = driver.find_element(By.XPATH, "//h1[@automation-id='user-head-not-nickname']").text
+            name_check = driver.find_element(By.XPATH, "//span[@automation-id='user-header-full-name']").text
       except NoSuchElementException:
             name_check = ""
 
@@ -166,13 +162,7 @@ for pi in popular_investors:
             print("failed crawl: " + pi)
 
 
-# driver.quit()
-print("Report: \n" +
-      "Crawled: " + str(number_of_record) + "/" + str(len(popular_investors)) + "\n" +
-      "Success Rate: " + str(number_of_record*100/len(popular_investors)) + "\n" +
-      "Failed pages: " + fail_pages + "\n" +
-      "Stop at: " + final_profile)
-
+driver.quit()
 print("Report: \n" +
       "Crawled: " + str(number_of_record) + "/" + str(len(popular_investors)) + "\n" +
       "Success Rate: " + str(number_of_record*100/len(popular_investors)) + "\n" +
